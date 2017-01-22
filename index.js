f36723a96b0fbe4171adc0a8d60fe768eb204d1b
Chris Jeganathan <chris.jeganathan@gmail.com>
2017-01-22 22:32:21 +0000

    BT homehub v0.1.0

---
+++ index.js
@@ -0,0 +1,33 @@
+var request = require('request-promise');
+var cheerio = require('cheerio');
+var ipAddress = 'http://192.168.1.254/';
+
+var init = function(ipadd) {
+	if(ipadd) ipAddress = ipadd;
+	return actions;
+};
+
+var getConnectedDevices = function(){
+	let devices = {};
+	return request(ipAddress)
+		.then(function (html) {
+	    var $ = cheerio.load(html);
+	    $('td').each(function(){
+	    	if($(this).attr('width') === '25%' && 
+	    		$(this).prev().attr('width') === $(this).next().attr('width') &&
+	    		$(this).text() !== 'No devices detected') {
+							let device = $(this).text();
+							let mac = $(this).next().text();
+							let ip = $(this).next().next().text();
+							devices[device.substr(0,10)] = {mac, ip};
+	    		}
+	    });
+		  return devices;
+		});
+};
+
+var actions = {
+	getConnectedDevices: getConnectedDevices
+};
+
+module.exports = init;
